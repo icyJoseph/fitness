581825eec0d028bf6a689ffc061ab315fda9bfaa
joseph_ctu <sephxd1234@gmail.com>
2017-12-04 18:43:09 +0100

    feat: button component

---
+++ components/TextButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+
+const TextButton = ({ children, onPress }) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Text>{children}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default TextButton;
