94b93cc617e8ea60f9a472402b75e34dfa154823
joseph_ctu <sephxd1234@gmail.com>
2017-12-04 17:24:24 +0100

    refactor: reorganized date format to YY/MM/DD

--- utils/helpers.js
+++ utils/helpers.js
@@ -122,7 +122,7 @@ export function calculateDirection(heading) {
 export function timeToString(time = Date.now()) {
   const date = new Date(time);
   const todayUTC = new Date(
-    Date.UTC(date.getDate(), date.getMonth(), date.getFullYear())
+    Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
   );
   return todayUTC.toISOString().split("T")[0];
 }
