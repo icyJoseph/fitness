b4fb583729c61fab79ad4553b61c70a6ae8b7297
joseph_ctu <sephxd1234@gmail.com>
2017-12-05 19:56:37 +0100

    feat: Daily reminder function

--- utils/helpers.js
+++ utils/helpers.js
@@ -126,3 +126,9 @@ export function timeToString(time = Date.now()) {
   );
   return todayUTC.toISOString().split("T")[0];
 }
+
+export function getDailyReminderValue() {
+  return {
+    today: "👋 Don't forget to log your data today"
+  };
+}
