3e92d3e06832ff2e21fe2630a6fd78d580db239f
joseph_ctu <sephxd1234@gmail.com>
2017-12-17 14:34:39 +0100

    style: centered submit button and aligned icons

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
-import { View, TouchableOpacity, Text } from "react-native";
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Platform
+} from "react-native";
 import { connect } from "react-redux";
 import DateHeader from "./DateHeader";
 import FitnessSlider from "./FitnessSlider";
@@ -13,11 +19,17 @@ import {
 import { Ionicons } from "@expo/vector-icons";
 import { submitEntry, removeEntry } from "../utils/api";
 import { addEntry } from "../actions";
+import { white, purple } from "../utils/colors";
 
 const SubmitBtn = ({ onPress }) => {
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text>Submit</Text>
+    <TouchableOpacity
+      style={
+        Platform.OS === "ios" ? styles.iosSubmitBtn : styles.androidSubmitBtn
+      }
+      onPress={onPress}
+    >
+      <Text style={styles.submitBtnText}>Submit</Text>
     </TouchableOpacity>
   );
 };
@@ -109,13 +121,13 @@ class AddEntry extends Component {
       );
     }
     return (
-      <View>
+      <View style={styles.container}>
         <DateHeader date={date.toLocaleDateString()} />
         {Object.keys(metaInfo).map(key => {
           const { getIcon, type, ...rest } = metaInfo[key];
           const value = this.state[key];
           return (
-            <View key={key}>
+            <View key={key} style={styles.row}>
               {getIcon()}
               {type === "slider" ? (
                 <FitnessSlider
@@ -139,6 +151,43 @@ class AddEntry extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white
+  },
+  row: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center"
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40
+  },
+  androidSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: "center"
+  }
+});
 
 function mapStateToProps(state) {
   const key = timeToString();
