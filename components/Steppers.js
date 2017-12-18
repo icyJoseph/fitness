57cf7b79ba9cc797f4ec422068ef1fac0904a8c1
joseph_ctu <sephxd1234@gmail.com>
2017-12-18 18:25:57 +0100

    style: added style for buttons and values

--- components/Steppers.js
+++ components/Steppers.js
@@ -7,21 +7,34 @@ import {
   Platform
 } from "react-native";
 import { FontAwesome, Entypo } from "@expo/vector-icons";
+import { white, gray, purple } from "../utils/colors";
 
 const Steppers = ({ max, unit, step, value, onIncrement, onDecrement }) => {
   return (
-    <View style={(styles.row, { justifyContent: "space-between" })}>
+    <View style={[styles.row, { justifyContent: "space-between" }]}>
       <View style={{ flexDirection: "row" }}>
-        <TouchableOpacity onPress={onDecrement}>
-          <FontAwesome name="minus" size={30} color={"black"} />
+        <TouchableOpacity
+          style={[
+            styles.iosBtn,
+            { borderTopRightRadius: 0, borderBottomRightRadius: 0 }
+          ]}
+          onPress={onDecrement}
+        >
+          <FontAwesome name="minus" size={30} color={purple} />
         </TouchableOpacity>
-        <TouchableOpacity onPress={onIncrement}>
-          <FontAwesome name="plus" size={30} color={"black"} />
+        <TouchableOpacity
+          style={[
+            styles.iosBtn,
+            { borderTopLeftRadius: 0, borderBottomRightRadius: 0 }
+          ]}
+          onPress={onIncrement}
+        >
+          <FontAwesome name="plus" size={30} color={purple} />
         </TouchableOpacity>
       </View>
-      <View>
-        <Text>{value}</Text>
-        <Text>{unit}</Text>
+      <View style={styles.metricCounter}>
+        <Text style={{ fontSize: 24, textAlign: "center" }}>{value}</Text>
+        <Text style={{ fontSize: 18, color: gray }}>{unit}</Text>
       </View>
     </View>
   );
@@ -33,6 +46,19 @@ const styles = StyleSheet.create({
   row: {
     flexDirection: "row",
     flex: 1,
+    alignItems: "center"
+  },
+  iosBtn: {
+    backgroundColor: white,
+    borderColor: purple,
+    borderRadius: 3,
+    padding: 5,
+    paddingLeft: 25,
+    paddingRight: 25
+  },
+  metricCounter: {
+    width: 85,
+    justifyContent: "center",
     alignItems: "center"
   }
 });
