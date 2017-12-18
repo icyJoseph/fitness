d7e0e0f22f26ee4bfc52e62c77ebbf8d78b163d1
joseph_ctu <sephxd1234@gmail.com>
2017-12-18 18:50:15 +0100

    style: added row, slider and metricCounter styles

--- components/FitnessSlider.js
+++ components/FitnessSlider.js
@@ -1,20 +1,37 @@
 import React from "react";
-import { View, Slider, Text } from "react-native";
+import { View, Slider, Text, StyleSheet } from "react-native";
+import { gray } from "../utils/colors";
 
 const FitnessSlider = ({ max, unit, step, value, onChange }) => {
   return (
-    <View>
+    <View style={styles.row}>
       <Slider
+        style={{ flex: 1 }}
         step={step}
         value={value}
         maximumValue={max}
         minimumValue={0}
         onValueChange={onChange}
       />
-      <Text>{value}</Text>
-      <Text>{unit}</Text>
+      <View style={styles.metricCounter}>
+        <Text style={{ fontSize: 24, textAlign: "center" }}>{value}</Text>
+        <Text style={{ fontSize: 18, color: gray }}>{unit}</Text>
+      </View>
     </View>
   );
 };
 
 export default FitnessSlider;
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center"
+  },
+  metricCounter: {
+    width: 85,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
