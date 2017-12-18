a0854e703f6fa51e8701e420582a348e8f58e5e9
joseph_ctu <sephxd1234@gmail.com>
2017-12-18 18:55:54 +0100

    style: oclor and style prop added to button

--- components/TextButton.js
+++ components/TextButton.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
+import { purple } from "../utils/colors";
 
-const TextButton = ({ children, onPress }) => {
+const TextButton = ({ children, onPress, style = {} }) => {
   return (
     <TouchableOpacity onPress={onPress}>
-      <Text>{children}</Text>
+      <Text style={[styles.reset, style]}>{children}</Text>
     </TouchableOpacity>
   );
 };
 
 export default TextButton;
+
+const styles = StyleSheet.create({
+  reset: {
+    textAlign: "center",
+    color: purple
+  }
+});
