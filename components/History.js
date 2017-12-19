40f92deff21c1e758043cfeb979c3793291b437c
joseph_ctu <sephxd1234@gmail.com>
2017-12-19 13:13:07 +0100

    fix: correctly implemented promise responses and added styles

--- components/History.js
+++ components/History.js
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  TouchableOpacity
+} from "react-native";
 import { connect } from "react-redux";
 import { receiveEntries, addEntry } from "../actions";
 import { timeToString, getDailyReminderValue } from "../utils/helpers";
 import { fetchCalendarResults } from "../utils/api";
 import UdaciFitnessCalendar from "udacifitness-calendar";
+import { white } from "../utils/colors";
+import DateHeader from "./DateHeader";
 
 class History extends Component {
   componentDidMount() {
     const { dispatch } = this.props;
-
     fetchCalendarResults()
       .then(entries => dispatch(receiveEntries(entries)))
-      .then(entries => {
-        if (!entries[timeToString]) {
+      .then(({ entries }) => {
+        if (!entries[timeToString()]) {
           dispatch(
             addEntry({
               [timeToString()]: getDailyReminderValue()
@@ -32,7 +39,7 @@ class History extends Component {
   }
 
   renderItem = ({ today, ...metrics }, formattedDate, key) => (
-    <View>
+    <View style={styles.item}>
       {today ? (
         <Text> {JSON.stringify(today)}</Text>
       ) : (
@@ -60,3 +67,22 @@ function mapStateToProps(entries) {
 }
 
 export default connect(mapStateToProps)(History);
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === "ios" ? 16 : 2,
+    padding: 18,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: "center",
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: "rgba(0,0,0,0.24)",
+    shadowOffset: {
+      width: 0,
+      height: 0
+    }
+  }
+});
