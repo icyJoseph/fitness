8765e176c619343c2589846d410ba8f7e509e455
joseph_ctu <sephxd1234@gmail.com>
2017-12-21 22:28:02 +0100

    feat: connected entry button to navigation stack

--- components/History.js
+++ components/History.js
@@ -56,7 +56,11 @@ class History extends Component {
           <Text style={styles.noDataText}>{today}</Text>
         </View>
       ) : (
-        <TouchableOpacity onPress={() => console.log("pressed")}>
+        <TouchableOpacity
+          onPress={() =>
+            this.props.navigation.navigate("EntryDetail", { entryId: key })
+          }
+        >
           <MetricCard metrics={metrics} date={formattedDate} />
         </TouchableOpacity>
       )}
