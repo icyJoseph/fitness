a9d229dd302e456a78438554b051b6f8906660a9
joseph_ctu <sephxd1234@gmail.com>
2017-12-21 22:09:13 +0100

    feat: Implemented detailed component

---
+++ components/EntryDetail.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class EntryDetail extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Entry Detail</Text>
+      </View>
+    );
+  }
+}
+
+export default EntryDetail;
