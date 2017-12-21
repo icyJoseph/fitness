e86ac8fe1f2960d32cbe290e046b313e58c68298
joseph_ctu <sephxd1234@gmail.com>
2017-12-21 17:50:14 +0100

    feat: added tabNavigator definition

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Slider } from "react-native";
+import { StyleSheet, Text, View, Slider, Platform } from "react-native";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { TabNavigator } from "react-navigation";
 import reducer from "./reducers";
 import AddEntry from "./components/AddEntry";
 import History from "./components/History";
+import { purple, white } from "../utils/colors";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+
+const Tabs = TabNavigator({
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: "History",
+      tabBarIcon: ({ tintColor }) => (
+        <Iconicons name="ios-bookmarks" size={30} color={tintColor} />
+      )
+    },
+    AddEntry: {
+      screen: AddEntry,
+      tabBarLabel: "Add Entry",
+      tabBarIcon: ({ tintColor }) => (
+        <FontAwesome name="plus-square" size={30} color={tintColor} />
+      )
+    }, {
+      tabBarOptions: {
+        activeTintColor: Platform.OS === 'ios' ? purple : white,
+        style: {
+          height: 56,
+          backgroundColor: Platform.OS === 'ios' ? white: purple,
+          shadowColor: 'rgba(0,0,0,0.24)',
+          shadowOffset: {
+            width: 0,
+            height: 3
+          }, 
+          shadowRadius: 6,
+          shadowOpacity: 1
+        }
+      }
+    }
+  }
+});
 
 export default class App extends Component {
   render() {
