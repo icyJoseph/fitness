0e54c8ac762db571c76d92d693df597f35643449
joseph_ctu <sephxd1234@gmail.com>
2017-12-22 14:26:48 +0100

    feat: implemented navigation and date

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { connect } from "react-redux";
+import { white } from "../utils/colors";
 
 class EntryDetail extends Component {
+  static navigationOptions = ({ navigation }) => {
+    const { entryId } = navigation.state.params;
+    const year = entryId.slice(0, 4);
+    const month = entryId.slice(5, 7);
+    const day = entryId.slice(8);
+    return {
+      title: `${month}/${day}/${year}`
+    };
+  };
+
   render() {
     return (
       <View>
-        <Text>Entry Detail</Text>
+        <Text>Entry Detail - {this.props.navigation.state.params.entryId}</Text>
       </View>
     );
   }
