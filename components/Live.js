b2d206dfd5c92d0f2968d10ecc7f40604c2fc668
joseph_ctu <sephxd1234@gmail.com>
2017-12-22 15:28:44 +0100

    feat: undetermined permission UI

--- components/Live.js
+++ components/Live.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
-import { View, Text, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  TouchableOpacity,
+  StyleSheet
+} from "react-native";
+import { Foundation } from "@expo/vector-icons";
+import { purple, white } from "../utils/colors";
 
 class Live extends Component {
   state = {
     coords: null,
-    status: null,
+    status: "undetermined",
     director: ""
   };
+
+  askPermission = () => {};
+
   render() {
     const { status, coords, direction } = this.state;
 
@@ -24,18 +35,53 @@ class Live extends Component {
 
     if (status == "undetermined") {
       return (
-        <View>
-          <Text>Undetermined</Text>
+        <View style={styles.center}>
+          <Foundation name="alert" size={50} />
+          <Text>You need to enable location services for this app.</Text>
+          <TouchableOpacity
+            onPress={() => {
+              this.askPermission;
+            }}
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}>Enable</Text>
+          </TouchableOpacity>
         </View>
       );
     }
 
     return (
       <View>
-        <Text>Live </Text>
+        <Text>Live</Text>
+        <Text>{JSON.stringify(this.state)}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between"
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 30,
+    marginRight: 30
+  },
+  button: {
+    padding: 10,
+    backgroundColor: purple,
+    alignSelf: "center",
+    borderRadius: 5,
+    margin: 20
+  },
+  buttonText: {
+    color: white,
+    fontSize: 20
+  }
+});
 
 export default Live;
